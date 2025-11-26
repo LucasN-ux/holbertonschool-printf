5c2e6bc5c2fd224a9051536eb0dac3fd33520c43
aourzik <a.ourzik@gmail.com>
2025-11-26 14:29:50 +0100

    fix: add print_int prototype

--- main.h
+++ main.h
@@ -14,8 +14,8 @@ typedef struct specifier
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
+int print_int(va_list args);
 
 int _printf(const char *format, ...);
-
 
 #endif
